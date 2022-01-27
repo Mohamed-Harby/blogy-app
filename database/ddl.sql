9f89f8c81de915f2d6efb51fe0eba0d9ed752261
Mohamed Harby <71990569+Mohamed-Harby@users.noreply.github.com>
2022-01-27 22:57:33 +0200

    Update ddl.sql

--- database/ddl.sql
+++ database/ddl.sql
@@ -9,16 +9,21 @@ create table user
     username varchar(20) not null,
     email    varchar(20) not null,
     password varchar(30) not null,
-    photo blob,
+    photo    blob,
     primary key (id)
 );
 
 create table post
 (
-    id      int auto_increment,
-    user_id int not null,
-    title   varchar(50),
-    content varchar(1000),
+    id              int auto_increment,
+    user_id         int not null,
+    title           varchar(50),
+    content         varchar(1000),
+    idea_emotion    int default 0,
+    like_emotion    int default 0,
+    dislike_emotion int default 0,
+    visibility      varchar(10) check (visibility in ('private', 'public')) not null,
+    date datetime   default now(),
     primary key (id),
     foreign key (user_id) references user (id) on delete cascade
 );
@@ -34,10 +39,12 @@ create table emotion
 );
 
 
-create table friendship(
-    first_friend_id int,
+
+create table friendship
+(
+    first_friend_id  int,
     second_friend_id int,
     primary key (first_friend_id, second_friend_id),
-    foreign key (first_friend_id) references user(id),
-    foreign key (second_friend_id) references user(id)
-)
+    foreign key (first_friend_id) references user (id),
+    foreign key (second_friend_id) references user (id)
+);
